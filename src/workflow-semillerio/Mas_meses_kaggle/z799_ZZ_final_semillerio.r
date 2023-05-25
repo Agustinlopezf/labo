a3ff57f0446db36f5ed656c7afc2c6ed0a5fdae4
Agustinlopezf <agustin.lopezfredes@gmail.com>
2023-05-25 17:18:07 -0300

    Mas meses kaggle

--- src/workflow-semillerio/Mas_meses_kaggle/z799_ZZ_final_semillerio.r
+++ src/workflow-semillerio/Mas_meses_kaggle/z799_ZZ_final_semillerio.r
@@ -25,7 +25,7 @@ PARAM$exp_input  <- "HT7510_kaggle_masmeses"
 
 #Atencion, que cada modelos se procesa con 5 semillas, ajuste a SUS necesidades
 PARAM$modelos_rank  <- c( 1,2,3,4,5,6,7,8,9,10 )  #Que modelos quiero, segun su posicion en el ranking e la Bayesian Optimizacion, ordenado por ganancia descendente
-PARAM$semillerio  <- 20   # cantidad de semillas a utilizar en el semillerio, podrian ser 50 o 100 para mayor estabilidad
+PARAM$semillerio  <- 50   # cantidad de semillas a utilizar en el semillerio, podrian ser 50 o 100 para mayor estabilidad
 PARAM$semilla_primos  <- 328789  # se utiliza para generar el vector de  PARAM$semillerio  semillas
 
 PARAM$kaggle$envios_desde  <-  8000L
